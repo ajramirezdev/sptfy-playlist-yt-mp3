09aa390b4c525cf9ed008dfaeaf55771e4827230
ae_jay <ajlimramirez@gmail.com>
2024-09-11 17:36:56 +0800

    improvements

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import puppeteer from "puppeteer";
+import * as cheerio from "cheerio";
 import inquirer from "inquirer";
+import { setTimeout } from "node:timers/promises";
 import "dotenv/config";
+
+const tracks = [];
 
 const getPlaylistSongs = async () => {
     const browser = await puppeteer.launch({
@@ -24,18 +28,63 @@ const getPlaylistSongs = async () => {
 
     await page.locator('[id="login-button"]').click();
 
-    await page.waitForNavigation();
-
     console.log("Login successful!");
 
-    // const answer = await inquirer.prompt([
-    //     {
-    //         type: "list",
-    //         name: "playlist",
-    //         message: "Select playlist:",
-    //         choices: [""],
-    //     },
-    // ]);
+    await setTimeout(5000);
+
+    const content = await page.content();
+    const $ = cheerio.load(content);
+
+    const $yourLibraryList = $(
+        'ul[aria-label="Your Library"] > div:first > div:eq(1) > li'
+    );
+
+    const playlists = [];
+
+    $yourLibraryList.each((index, element) => {
+        const playlist = $(element).find("p:first").text();
+        playlists.push(playlist);
+    });
+
+    const answer = await inquirer.prompt([
+        {
+            type: "list",
+            name: "playlist",
+            message: "Select playlist:",
+            choices: playlists,
+        },
+    ]);
+
+    const selectedPlaylistIndex = playlists.findIndex(
+        (playlist) => playlist === answer.playlist
+    );
+
+    await page
+        .locator(
+            `[aria-label="Your Library"] > div > div:nth-child(2) > li:nth-child(${
+                selectedPlaylistIndex + 1
+            }) > div > [role="button"]`
+        )
+        .click();
+
+    console.log(`Fetching tracks from the playlist...`);
+
+    await setTimeout(2000);
+
+    const updatedContent = await page.content();
+    const updated$ = cheerio.load(updatedContent);
+
+    const $tracklist = updated$(
+        'div[data-testid="playlist-tracklist"] > div:nth-child(2) > div:nth-child(2) > [role="row"]'
+    );
+
+    $tracklist.each((index, element) => {
+        const track = $(element).find("a:first").text();
+        const artist = $(element).find("a:eq(1)").text();
+        tracks.push(`${track} - ${artist} audio`);
+    });
+
+    console.log(tracks);
 };
 
 getPlaylistSongs();
